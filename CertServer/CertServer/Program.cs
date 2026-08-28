f507f6278adae5e5d31395912028ba331a3b4aed
Erik Volger <erikvolger1970@gmail.com>
2026-08-28 15:14:10 +0200

    added MudBlazor configuration

--- CertServer/CertServer/Program.cs
+++ CertServer/CertServer/Program.cs
@@ -1,9 +1,12 @@
 using CertServer.Client.Pages;
 using CertServer.Components;
+using MudBlazor.Services;
 
 var builder = WebApplication.CreateBuilder(args);
 
 builder.AddServiceDefaults();
+
+builder.Services.AddMudServices();
 
 // Add services to the container.
 builder.Services.AddRazorComponents()
@@ -25,6 +28,7 @@ else
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
+
 app.UseStatusCodePagesWithReExecute("/not-found", createScopeForStatusCodePages: true);
 app.UseHttpsRedirection();
 
